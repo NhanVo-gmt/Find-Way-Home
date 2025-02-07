e0d63c8acfd44f89046a515ec396a7e7fe4e3468
NhanVoo <thanhnhan@osogames.studio>
2025-02-07 15:35:42 +0700

    adding back btn

---
+++ Assets/Scripts/Level/BackButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : ButtonBehaviour
+{
+    [Header("Back Button")]
+    [SerializeField] private List<GameObject> walls;
+    
+    protected override void OnPlayerPressed()
+    {
+        base.OnPlayerPressed();
+
+        foreach (var wall in walls)
+        {
+            wall.SetActive(false);
+        }
+    }
+
+    protected override void OnPlayerUnpressed()
+    {
+        
+    }
+}
